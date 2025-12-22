6189a9c36b8832aeded5a9a03644bdbf94b783bb
Richard Feder <richardfeder@MacBookPro.localdomain>
2025-12-22 15:33:04 -0500

    Reformat publications section: two-column layout, collapsible abstracts, smaller titles with hyperlinks

--- js/project-toggle.js
+++ js/project-toggle.js
@@ -19,3 +19,25 @@ function toggleProject(element) {
 
 // Make sure the function is available globally
 window.toggleProject = toggleProject;
+
+// Toggle publication abstracts
+function toggleAbstract(event, element) {
+  event.preventDefault();
+  const abstractDiv = element.previousElementSibling;
+  const preview = element.parentElement.querySelector('.abstract-preview');
+  
+  if (abstractDiv.classList.contains('show')) {
+    // Collapse
+    abstractDiv.classList.remove('show');
+    preview.style.display = 'block';
+    element.textContent = 'Read more';
+  } else {
+    // Expand
+    abstractDiv.classList.add('show');
+    preview.style.display = 'none';
+    element.textContent = 'Read less';
+  }
+}
+
+// Make sure the function is available globally
+window.toggleAbstract = toggleAbstract;
